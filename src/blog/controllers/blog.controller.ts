3ff3c53c004a3645fa035e86789031a1f439684a
Raghu Rana <raghurana@gmail.com>
2019-08-29 16:40:34 +1000

    First controller method added. Untested

--- src/blog/controllers/blog.controller.ts
+++ src/blog/controllers/blog.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Res, Body } from '@nestjs/common';
+import { BlogService } from '../services/blog.service';
+import { CreatePostDTO } from '../dto/create-post-dto';
 
 @Controller('blog')
-export class BlogController {}
+export class BlogController {
+
+    constructor(private blogService: BlogService) {}
+
+    @Post('/newpost')
+    async newPost(@Body() postDto: CreatePostDTO) {
+        const savedPost = await this.blogService.addPost(postDto);
+        return {
+            message: 'new post created successfully.',
+            post: savedPost,
+        };
+    }
+
+}
