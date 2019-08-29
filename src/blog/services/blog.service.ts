edb40365c97d52c8de0c6e02295ef6dc6ae48fd7
Raghu Rana <raghurana@gmail.com>
2019-08-29 16:30:24 +1000

    Add method added to the blog service

--- src/blog/services/blog.service.ts
+++ src/blog/services/blog.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Post } from '../interfaces/post.interface';
+import { CreatePostDTO } from '../dto/create-post-dto';
 
 @Injectable()
-export class BlogService {}
+export class BlogService {
+
+    constructor(@InjectModel('Post') private readonly postModel: Model<Post>) {}
+
+    async addPost(createPostDto: CreatePostDTO): Promise<Post> {
+        const newPost = await this.postModel.create(createPostDto);
+        return newPost.save();
+    }
+
+}
