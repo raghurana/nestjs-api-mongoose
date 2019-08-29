c0f42a917b81c8ce0e2dab1a350bd269b1d5c4cf
Raghu Rana <raghurana@gmail.com>
2019-08-29 16:31:59 +1000

    Blog controller added

--- src/blog/blog.module.ts
+++ src/blog/blog.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { BlogService } from './services/blog.service';
+import { BlogController } from './controllers/blog.controller';
 
 @Module({
-  providers: [BlogService]
+  providers: [BlogService],
+  controllers: [BlogController],
 })
 export class BlogModule {}
